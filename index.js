f5753ef3c479b41cd9a74d6cf44d73580e50a0fc
Jeremy Brenner <jeremybrenner0@gmail.com>
2017-04-07 11:14:20 -0700

    Requires, test get all popular decks

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Test');
+const hs = require('hearthpwn-scraper');
+const warriorOpt = heroes: ['warrior'];
+
+hs.getPopularDecks().then((decks) => {
+	console.log('\nThe PopularDecks');
+	console.log('=============================\n');
+	console.log(decks);
+});
