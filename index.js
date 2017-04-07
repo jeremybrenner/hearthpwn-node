ee4e1076e10936a1b059c67427eb87d5895d1526
Jeremy Brenner <jeremybrenner0@gmail.com>
2017-04-07 11:20:37 -0700

    Format response, add warrior option

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const hs = require('hearthpwn-scraper');
-const warriorOpt = heroes: ['warrior'];
+const warriorOpt = { heroes: ['warrior'] };
 
 hs.getPopularDecks().then((decks) => {
 	console.log('\nThe PopularDecks');
 	console.log('=============================\n');
-	console.log(decks);
+	decks.map((deck) => {
+		console.log('* Name : ' + deck.title);
+		console.log('* URL  : ' + deck.url);
+		console.log('------------------------');
+	});
 });
