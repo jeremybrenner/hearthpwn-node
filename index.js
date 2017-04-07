e5a79db78d86036ffe099f8ee369c84389927200
Jeremy Brenner <jeremybrenner0@gmail.com>
2017-04-07 16:34:30 -0700

    Handle and appropriately redirect response to More Info prompt

--- index.js
+++ index.js
@@ -22,30 +22,28 @@ commands
 	.option('-c, --classes [classes]', 'Filter decks by class', splitClasses)
 	.parse(process.argv);
 
-// if classes are passed, set them to option for deck fetch
-if(commands.classes) { 
-	options.heroes = commands.classes
-}
 
-// indicate search, log custom class options being passed
-console.log('\nSearching for decks...');
 
-if(options.heroes.length > 0) {
-	console.log('\nWith the following classes :');
-	options.heroes.map((c) => { 
-		console.log(' * ' + capitalize(c)) 
+const handleValid = () => {
+	prompt('\n Which deck would you like to know more about ? [' + decks[0].id + ' - ' + decks[decks.length-1].id + '] :', (response) => {
+		console.log(response);
 	});
 };
 
 const promptUser = () => {
-	const valid = ['Y','y','N','n'];
-	prompt('\n => Would you like to know more? (Y/N): ', (input) => {
+	const valid = ['y','yes','n','no'];
+	prompt('\n => Would you like to know more? (Y/N): ', (response) => {
+
+		let input = response.toLowerCase();
+
 		if (valid.indexOf(input) === -1) {
-			console.log('WRONG');
-			console.log(input);
+			console.log('\n * ERROR: Invalid input : ' + input + ' * ');
+			promptUser();
+		} else if(input === valid[2] || input === valid[3]) {
+			console.log('\n *** EXITING, BYE! ***');
+			process.exit();
 		} else {
-			console.log('CORRECT');
-			console.log(input);
+			handleValid(input);
 		}
 	})
 };
@@ -62,6 +60,22 @@ const logResults = () => {
 
 // fetch relevant decks from hearthpwn
 const fetchDecks = () => {
+
+	// if classes are passed, set them to option for deck fetch
+	if(commands.classes) { 
+		options.heroes = commands.classes
+	}
+
+	// indicate search, log custom class options being passed
+	console.log('\nSearching for decks...');
+
+	if(options.heroes.length > 0) {
+		console.log('\nWith the following classes :');
+		options.heroes.map((c) => { 
+			console.log(' * ' + capitalize(c)) 
+		});
+	};
+
 	hs.getPopularDecks(options)
 	.then((results) => {
 		console.log('\n\n*** Search Results ***');
