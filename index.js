38882f76d9316a2e694d341a23ffb63df021fb21
Jeremy Brenner <jeremybrenner0@gmail.com>
2017-04-07 13:08:30 -0700

    Add helper to capitalize classes in logging, exit node process on completion

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const splitClasses = (val) => {
 	return val.split(',');
 } 
 
+// capitialize each class for logging
+const capitalize = (string) => {
+	return string.charAt(0).toUpperCase() + string.substring(1).toLowerCase();
+}
+
 // capture and parse command arguments
 commands
 	.version('0.0.1')
@@ -18,17 +23,31 @@ if(commands.classes) {
 	options.heroes = commands.classes
 }
 
+// indicate search, log custom class options being passed
 console.log('\nSearching for decks...');
+
 if(options.heroes.length > 0) {
-	console.log(' With the following classes => ')
+	console.log('\nWith the following classes :');
+	options.heroes.map((c) => { 
+		console.log(' * ' + capitalize(c)) 
+	});
 };
+
 // fetch relevant decks from hearthpwn
-hs.getPopularDecks(options).then((decks) => {
-	console.log('\n\n*** The Popular Decks ***');
+hs.getPopularDecks(options)
+.then((decks) => {
+	console.log('\n\n*** Search Results ***');
 	console.log('=============================\n\n');
 	decks.map((deck) => {
 		console.log('* Name : ' + deck.title);
 		console.log('* URL  : ' + deck.url);
 		console.log('------------------------');
 	});
-});
+})
+.then(()=> {
+	console.log('\n\n*** EXITING ***\n\n');
+	// shut down node process
+	process.exit();
+})
+
+
