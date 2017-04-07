c1cfe79e132a727751e554d86684ab6667480218
Jeremy Brenner <jeremybrenner0@gmail.com>
2017-04-07 12:41:21 -0700

    Add options object to pass for deck fetching

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const hs      = require('hearthpwn-scraper');
-const command = require('commander');
+const commands = require('commander');
+const options = { heroes: [] };
 
+// split class flag arguments
+// const splitClasses = (val) => {
+// 	return val.split(',');
+// } 
+
+// capture and parse command arguments
+// commands
+// 	.version('0.0.1')
+// 	.option('-c, --classes [classes]', 'Filter decks by class', splitClasses)
+// 	.parse(process.argv);
+
+// if classes are passed, set them to option for deck fetch
+// if(commands.classes) { 
+// 	options.heroes = commands.classes
+// }
+
+// fetch relevant decks from hearthpwn
 hs.getPopularDecks().then((decks) => {
-	console.log('\n\n*** The Popular Decks ***');
-	console.log('=============================\n\n');
+	// console.log('\n\n*** The Popular Decks ***');
+	// console.log('=============================\n\n');
 	decks.map((deck) => {
-		console.log('* Name : ' + deck.title);
-		console.log('* URL  : ' + deck.url);
-		console.log('------------------------');
+		// console.log('* Name : ' + deck.title);
+		// console.log('* URL  : ' + deck.url);
+		// console.log('------------------------');
 	});
 });
