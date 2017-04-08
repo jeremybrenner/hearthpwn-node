f411e7e3111ea8e3e31f2dc306cb74386c240212
Jeremy Brenner <jeremybrenner0@gmail.com>
2017-04-07 17:03:33 -0700

    Fetch and display valid deck details, then prompt for user action

--- index.js
+++ index.js
@@ -6,36 +6,46 @@ let options  = { heroes: [] };
 let decks = [];
 let count = 0;
 
+const finalPrompt = () => {
+	// either exit or display deck list again
+};
+
 const handleValid = () => {
 	const first = parseInt(decks[0].id);
 	const last  = parseInt(decks[decks.length-1].id);
-	prompt('\n Which deck would you like to know more about ? [' + first + ' - ' + last + '] : ', (response) => {
+	prompt('\n Which deck would you like to know more about ? [' + first + ' - ' + last + '] : \n\n', (response) => {
 		const intId = parseInt(response) || 0;
-		if(intId <= 0 || intId > last) {
+		if (intId <= 0 || intId > last) {
 			console.log('* ERROR: Please enter a valid deck ID [' + first + ' - ' + last + '] : ');
 			handleValid();
 		}else {
-			console.log('Valid option');
-			console.log(intId)
-			console.log(decks[intId]);
 			let url = decks[intId].url;
+			hs.getDeckInfo(url).then((deck) => {
+				console.log('\n\n     *** Deck List ***      ');
+				console.log('=============================\n');
+				deck.map((c) => {
+					console.log('* ' + c.card + '  x (' + c.quantity + ') ');
+				});
+				console.log('\n=============================\n\n');
+				finalPrompt();
+			});
 		}
 	});
 };
 
 const promptUser = () => {
 	const valid = ['y','yes','n','no'];
-	prompt('\n => Would you like to know more? (Y/N): ', (response) => {
+	prompt('\n => Would you like to know more? (Y/N): \n\n', (response) => {
 
 		let input = response.toLowerCase();
 
 		if (valid.indexOf(input) === -1) {
 			console.log('\n * ERROR: Invalid input : ' + input + ' * ');
 			promptUser();
-		} else if(input === valid[2] || input === valid[3]) {
+		}else if(input === valid[2] || input === valid[3]) {
 			console.log('\n *** EXITING, BYE! ***');
 			process.exit();
-		} else {
+		}else {
 			handleValid(input);
 		}
 	})
@@ -74,7 +84,7 @@ const fetchDecks = () => {
 
 	hs.getPopularDecks(options)
 	.then((results) => {
-		console.log('\n\n*** Search Results ***');
+		console.log('\n\n   *** Search Results ***    ');
 		console.log('=============================\n\n');
 		decks = results.map((deck) => {
 			let rdyDeck = {};
